41441776c9ceeb344b40cdd0c671369c5ca4372e
David Dietrick <Ddietrick@rebarsys.com>
2020-02-04 14:39:24 -0500

    Added navigation, worked on about me section

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,11 @@ export default {
     let faScript = document.createElement('script'); 
     faScript.setAttribute('src', 'https://kit.fontawesome.com/a39a4bd9a8.js');
     document.head.appendChild(faScript);
+
+    let fontSheet = document.createElement('link');
+    fontSheet.setAttribute('href', 'https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900');
+    fontSheet.setAttribute('rel', 'stylesheet');
+    document.head.append(fontSheet);
   }
 }
 </script>
